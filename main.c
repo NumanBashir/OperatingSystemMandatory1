2722079a0da924c2d539be69f4194c7fbf54e113
NumanBashir <sportybashir@gmail.com>
2022-09-25 22:23:54 +0200

    commands done

--- main.c
+++ main.c
@@ -23,37 +23,11 @@ int execute_command(char **input) {
 
     if (pid == 0) {
         execvp(input[0], input);
-    } else if(strcmp(input[0],"exit") == 0) {
+    }
 
     waitpid(pid, NULL, 0);
     return 0;
 
-
-    //printf("%s", input);
-    /*if(!strcmp(input, "ls")) {
-        execlp(input, "-a", NULL);
-    } else if(!strcmp(input, "exit")) {
-        printf("Program exited");
-    } else if(!strcmp(input, "pwd")) {
-        execlp("pwd", "pwd", NULL);
-        printf("%s\n", cwd);
-    } else if(!strcmp(input, "cd")) {
-        printf("please enter argument\n");
-    } else if(!strcmp(input, "cd ..")) {
-        chdir("..");
-    } else if(!strcmp(input, "ping")) {
-        execlp("ping", "ping", "google.dk", NULL);
-    } else {
-        printf("command not found: %s\n", input);
-    }*/
-
-    //test
-
-    /*if(!strcmp(input, input)) {
-        execlp(input, input, NULL);
-    } else {
-        printf("command not found: %s\n", input);
-    }*/
 
 }
 
@@ -71,15 +45,20 @@ int main() {
         char **string = tokenization(input);
 
 
+
         if(!strcmp(string[0], "ls") | !strcmp(string[0], "pwd") | !strcmp(string[0], "ping")) {
             execute_command(string);
         } else if(strcmp(string[0], "exit") == 0) {
             exit(0);
         }
 
-    }
+        if(!strcmp(string[0], "cd")) {
+            chdir(string[1]);
+        }
 
         //pid = fork();
+
+        //Pipe
 
 
     }
